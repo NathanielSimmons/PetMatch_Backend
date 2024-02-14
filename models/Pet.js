933d06f1f71cb751124f41b65618bc0636dedd25
NathanielSimmons <nathaniel.cj.simmons@gmail.com>
2024-02-14 11:32:45 -0500

    updated pet model for match functionality

--- models/Pet.js
+++ models/Pet.js
@@ -17,8 +17,13 @@ const petSchema = new mongoose.Schema({
     ref: 'User',
     required: true
   },
-  pictures: String
+  pictures: String,
+  likedBy: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }]
 }, { timestamps: true });
+
 
 const Pet = mongoose.model('Pet', petSchema);
 
